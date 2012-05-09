a867928cde26d3b967a0001a5a541e65543cd000
Henning Pridöhl <henning@hprid.de>
2012-05-09 17:45:06 +0200

    Add Makefile to build debian package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='adminapi',
+    url='http://serveradmin.innogames.de/',
+    author='Henning Pridoehl',
+    author_email='henning.pridoehl@innogames.de',
+    packages=['adminapi', 'adminapi.dataset'],
+    version='0.1',
+    long_description=('Admin remote API for querying servers and making'
+                      'API requests')
+)
