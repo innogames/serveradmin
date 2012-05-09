4a1145097e2065696b3c24b6483abb1b1ac58bfb
Henning Pridöhl <henning@hprid.de>
2012-05-09 12:19:13 +0200

    Finish dataset query api view

--- serveradmin/api/views.py
+++ serveradmin/api/views.py
@@ -1,5 +1,7 @@
 from serveradmin.api.decorators import api_view
-from serveradmin.dataset import query
+from serveradmin.dataset.base import lookups
+from serveradmin.dataset import QuerySet
+from serveradmin.dataset.filters import filter_from_obj
 
 @api_view
 def echo(request, app, data):
@@ -7,12 +9,24 @@ def echo(request, app, data):
 
 @api_view
 def dataset_query(request, app, data):
-    # FIXME: Replace dummy with real action
-    q = query(hostname='techerror.support')
+    if not all(x in data for x in ('filters', 'restrict', 'augmentations')):
+        raise ValueError('Invalid query object')
+    filters = {}
+    for attr, filter_obj in data['filters'].iteritems():
+        filters[attr] = filter_from_obj(filter_obj)
+    q = QuerySet(filters=filters)
+    if data['restrict']:
+        q.restrict(*data['restrict'])
+    if data['augmentations']:
+        q.augment(*data['augmentations'])
+
     return {
-        'success': True,
+        'status': 'success',
         'servers': q.get_raw_results(),
-        'convert_set': [attr.name for attr in q.get_attr_lookup().itervalues()]
+        'convert_set': [attr.name for attr in lookups.attr_ids.itervalues()
+                        if attr.multi],
+        'convert_ip': [attr.name for attr in lookups.attr_ids.itervalues()
+                        if attr.type == 'ip']
     }
 
 @api_view
