fa268a4255af6b929844099e969303f307d627e5
Henning Pridöhl <henning@hprid.de>
2012-06-20 11:43:04 +0200

    Add missing exception classes

---
+++ adminapi/dataset/exceptions.py
@@ -0,0 +1,20 @@
+class DatasetError(Exception):
+    pass
+
+class CommitError(Exception):
+    pass
+
+class CommitValidationFailed(CommitError):
+    def __init__(self, message, violations=None):
+        CommitError.__init__(self, message)
+        if violations is None:
+            violations = []
+        self.violations = violations
+
+class CommitNewerData(CommitError):
+    def __init__(self, message, newer=None):
+        CommitError.__init__(self, message)
+        if newer is None:
+            newer = []
+        self.newer = newer
+
