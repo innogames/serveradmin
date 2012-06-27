89ef6af29686a4d77b6d62c6918f35c6b0d0de5e
Henning Pridöhl <henning@hprid.de>
2012-06-27 11:43:31 +0200

    Fix cyclic import in adminapi

--- adminapi/utils/json.py
+++ adminapi/utils/json.py
@@ -1,13 +1,13 @@
 from time import mktime
 from datetime import datetime, date, timedelta
 
-from adminapi.dataset.base import MultiAttr
 from adminapi.utils import IP
 
 def json_encode_extra(obj):
-    if isinstance(obj, MultiAttr):
+    # Proxied sets are used by MultiAttr
+    if hasattr(obj, '_proxied_set'):
         return list(obj._proxied_set)
-    if isinstance(obj, set):
+    elif isinstance(obj, set):
         return list(obj)
     elif isinstance(obj, IP):
         return obj.ip
