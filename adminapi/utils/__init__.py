2e4fec22e34bd5e2af4e293b9a94f3c5c5926bf6
Henning Pridöhl <henning@hprid.de>
2012-07-20 12:55:36 +0200

    Adminapi: Add Network class

--- adminapi/utils/__init__.py
+++ adminapi/utils/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import sys
 from itertools import izip
+from math import log
 
 def print_table(input_table_rows, max_col_len=40, file=sys.stdout):
     if not input_table_rows:
@@ -115,19 +116,69 @@ class IP(object):
         
         return '.'.join(reversed(ip))
 
+    __str__ = as_ip
+
     def as_int(self):
         return self.ip
 
     def is_internal(self):
-        return any(start <= self.ip <= end for start, end in PRIVATE_IP_BLOCKS)
+        return any(net.min_ip <= self.ip <= net.max_ip for net in PRIVATE_IP_BLOCKS)
 
     def is_public(self):
         return not self.is_internal()
 
+class Network(object):
+    def __init__(self, min_ip, max_ip=None):
+        if max_ip is None:
+            if isinstance(min_ip, basestring):
+                cidr_ip, size = min_ip.split('/')
+                num_ips = 1 << (32 - int(size))
+                self.min_ip = IP(cidr_ip)
+                self.max_ip = IP(self.min_ip.as_int() + num_ips - 1)
+            elif isinstance(list, tuple):
+                self.min_ip = IP(min_ip[0])
+                self.max_ip = IP(max_ip[0])
+        else:
+            self.min_ip = IP(min_ip) if not isinstance(min_ip, IP) else min_ip
+            self.max_ip = IP(max_ip) if not isinstance(max_ip, IP) else max_ip
+
+        if self.max_ip < self.min_ip:
+            raise ValueError('max_ip must be greater or equal min_ip')
+
+    def __hash__(self):
+        return hash(self.min_ip) ^ hash(self.max_ip)
+
+    def __eq__(self, other):
+        if isinstance(other, Network):
+            return self.min_ip == other.min_ip and self.max_ip == other.max_ip
+        return False
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def as_cidr(self):
+        num_ips = self.max_ip.as_int() - self.min_ip.as_int() + 1
+        power_of_two = (num_ips & (num_ips - 1)) == 0
+        if not power_of_two:
+            raise TypeError("Network can't be converted to cidr")
+        size = 32 - int(log(num_ips, 2))
+        return u'{0}/{1}'.format(self.min_ip.as_ip(), size)
+
+    def __repr__(self):
+        try:
+            return self.as_cidr()
+        except TypeError:
+            return u'Network({0!r}, {1!r})'.format(self.min_ip, self.max_ip)
+
+    def inside(self, ip):
+        return self.min_ip <= ip <= self.max_ip
+
 # http://en.wikipedia.org/wiki/Private_network
 PRIVATE_IP_BLOCKS = [
-    (IP('10.0.0.0').as_int(),  IP('10.255.255.255').as_int()),
-    (IP('172.16.0.0').as_int(), IP('172.31.255.255').as_int()),
-    (IP('192.168.0.0').as_int(), IP('192.168.255.255').as_int())
-]
+    Network('10.0.0.0/8'), Network('172.16.0.0/12'), Network('192.168.0.0/16')]
 
+PUBLIC_IP_BLOCKS = [
+    Network(IP('0.0.0.0'), IP('9.255.255.255')),
+    Network(IP('11.0.0.0'), IP('172.15.255.255')),
+    Network(IP('172.32.0.0'), IP('192.167.255.255')),
+    Network(IP('192.169.0.0'), IP('255.255.255.255'))]
