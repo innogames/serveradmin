ba7eb24b51272e848b899af11b3e51cf0ffd5560
Henning Pridöhl <henning@hprid.de>
2012-08-01 14:36:30 +0200

    servermonitor: Highlight CPU/IO values in overview

---
+++ serveradmin/servermonitor/templatetags/servermonitor.py
@@ -0,0 +1,25 @@
+from colorsys import hsv_to_rgb
+
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def format_percent(value, range_start, range_end, range_diff):
+    if value < range_start:
+        return u'{0}&nbsp;%'.format(value)
+
+    hue_start = 0
+    hue_end = 0.40
+    sat = 0.4
+    val = 1.0
+
+    part = 1 - ((value - range_start) / range_diff)
+    part = min(max(part, 0), 1.0)
+
+    hue = part * (hue_end - hue_start) + hue_start
+    r, g, b = hsv_to_rgb(hue, sat, val)
+    r, g, b = int(r * 255), int(g * 255), int(b * 255)
+
+    return (u'<div style="background:rgb({r}, {g}, {b});">'
+            '{value}&nbsp;%</div>').format(r=r, g=g, b=b, value=value)
