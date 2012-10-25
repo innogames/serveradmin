48969ba0d69e8f384abbee1e5ec8a31c6852c72f
Henning Pridöhl <henning@hprid.de>
2012-10-25 17:22:12 +0200

    dataset: Implement get_segments API

--- serveradmin/dataset/api.py
+++ serveradmin/dataset/api.py
@@ -1,7 +1,12 @@
 from serveradmin.api.decorators import api_function
-from serveradmin.dataset.models import ServerType
+from serveradmin.dataset.models import ServerType, Segment
 
 @api_function(group='dataset')
 def get_servertypes():
     """Returns a list of available servertypes"""
     return [stype.name for stype in ServerType.objects.all()]
+
+@api_function(group='dataset')
+def get_segments():
+    """Returns a list of available segments"""
+    return [seg.segment for seg in Segment.objects.all()]
