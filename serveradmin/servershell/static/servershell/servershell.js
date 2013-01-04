697371af63f3c38229e5f0e810ad5b7001c70a39
Henning Pridöhl <henning@hprid.de>
2013-01-04 15:52:29 +0100

    servershell: Remove pipe as separator

--- serveradmin/servershell/static/servershell/servershell.js
+++ serveradmin/servershell/static/servershell/servershell.js
@@ -257,7 +257,7 @@ function _make_attr_editable(cell, server, attr_name, value)
                 if ($.trim(unparsed_values) == '') {
                     unparsed_values = [];
                 } else {
-                    unparsed_values = unparsed_values.split(/[\s|,]+/);
+                    unparsed_values = unparsed_values.split(/[\s,]+/);
                 }
                 commit_data = {'action': 'multi', 'add': [], 'remove': []};
                 var edit_values = [];
