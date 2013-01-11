9687ac403ebe406e3a9e934244458baca4109516
Henning Pridöhl <henning@hprid.de>
2013-01-11 16:16:59 +0100

    iprange: Remove route from admin.py

--- serveradmin/iprange/admin.py
+++ serveradmin/iprange/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-from serveradmin.iprange.models import IPRange, Route, SegmentRoute
+from serveradmin.iprange.models import IPRange
 
 admin.site.register(IPRange)
-admin.site.register(Route)
-admin.site.register(SegmentRoute)
