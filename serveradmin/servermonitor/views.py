73e26c7414bae94d7dd2cd5f161e3bd4a75efbfa
Henning Pridöhl <henning@hprid.de>
2013-01-17 12:03:54 +0100

    servermonitor: Refactor servermonitor view (extraced informating fetching)

--- serveradmin/servermonitor/views.py
+++ serveradmin/servermonitor/views.py
@@ -17,9 +17,10 @@ from django.conf import settings
 from adminapi.utils.parse import parse_query
 from serveradmin.dataset import query, filters, DatasetError
 from serveradmin.dataset.models import Segment, ServerType
-from serveradmin.servermonitor.models import (GraphValue, ServerData,
-        get_available_graphs, get_graph_url, split_graph_name, join_graph_name,
-        reload_graphs, PERIODS, query_livegraph)
+from serveradmin.servermonitor.models import (get_available_graphs,
+        get_graph_url, split_graph_name, join_graph_name, reload_graphs,
+        PERIODS, query_livegraph)
+from serveradmin.servermonitor.getinfo import get_information
 
 @login_required
 @ensure_csrf_cookie
@@ -51,7 +52,7 @@ def index(request):
             if not matched_servers:
                 template_info.update({
                     'understood': host_query.get_representation().as_code(),
-                    'hardware_hosts': []
+                    'hosts': []
                 })
                 return TemplateResponse(request, 'servermonitor/index.html',
                         template_info)
@@ -86,96 +87,10 @@ def index(request):
                 host_data[what][period] = None
         hardware[hw_host['hostname']] = host_data
     hostnames = hardware.keys()
+    template_info.update(get_information(hostnames, hardware))
 
-    server_data = ServerData.objects.filter(hostname__in=hostnames).only(
-            'hostname', 'mem_free_dom0', 'mem_installed_dom0',
-            'disk_free_dom0', 'running_vserver').values()
-    graph_values = (GraphValue.objects.filter(hostname__in=hostnames,
-            graph_name__in=['cpu_dom0_value_max_95', 'io2_dom0_value_max_95'])
-            .values())
-    
-    # Annotate hardware with data from server data table
-    mem_free_sum = 0
-    mem_free_count = 0
-    mem_total_sum = 0
-    mem_total_count = 0
-    disk_free_sum = 0
-    disk_free_count = 0
-    
-    cpu_aggregate = {}
-    io_aggregate = {}
-    for period in periods:
-        cpu_aggregate[period] = {'sum': 0, 'count': 0}
-        io_aggregate[period] = {'sum': 0, 'count': 0}
-    
-    to_bytes = 1024 * 1024
-    for host_info in server_data:
-        if host_info['mem_installed_dom0']:
-            mem_total = host_info['mem_installed_dom0'] * to_bytes
-        else:
-            mem_total = None
-        hardware[host_info['hostname']].update({
-            'guests': host_info['running_vserver'].split(),
-            'mem_free': host_info['mem_free_dom0']* to_bytes,
-            'mem_total': mem_total,
-            'disk_free': host_info['disk_free_dom0']* to_bytes
-        })
-        
-        if host_info['mem_free_dom0']:
-            mem_free_sum += host_info['mem_free_dom0']
-            mem_free_count += 1
-        if host_info['mem_installed_dom0']:
-            mem_total_sum += host_info['mem_installed_dom0']
-            mem_total_count += 1
-        if host_info['disk_free_dom0']:
-            disk_free_sum += host_info['disk_free_dom0']
-            disk_free_count += 1
-    
-    # Annotate hardware with the values for cpu/io
-    for graph_value in graph_values:
-        if graph_value['graph_name'] == 'cpu_dom0_value_max_95':
-            hardware[graph_value['hostname']]['cpu'][graph_value['period']] = \
-                    int(round(graph_value['value']))
-            cpu_aggregate[graph_value['period']]['sum'] += graph_value['value']
-            cpu_aggregate[graph_value['period']]['count'] += 1
-        elif graph_value['graph_name'] == 'io2_dom0_value_max_95':
-            hardware[graph_value['hostname']]['io'][graph_value['period']] = \
-                    int(round(graph_value['value']))
-            io_aggregate[graph_value['period']]['sum'] += graph_value['value']
-            io_aggregate[graph_value['period']]['count'] += 1
-
-    hardware = hardware.values()
-    hardware.sort(key=itemgetter('hostname'))
-
-    mem_free_sum *= to_bytes
-    mem_total_sum *= to_bytes
-    disk_free_sum *= to_bytes
-    
-    for period in periods:
-        cpu_count = cpu_aggregate[period]['count']
-        cpu_count = cpu_count if cpu_count else 1
-        cpu_aggregate[period]['avg'] = round(cpu_aggregate[period]['sum'] /
-                cpu_count, 2)
-
-        io_count = io_aggregate[period]['count']
-        io_count = io_count if io_count else 1
-        io_aggregate[period]['avg'] = round(io_aggregate[period]['sum'] /
-                io_count, 2)
-    
-    mem_free_count = mem_free_count if mem_free_count else 1
-    mem_total_count = mem_total_count if mem_total_count else 1
-    disk_free_count = disk_free_count if disk_free_count else 1
     template_info.update({
-        'hardware_hosts': hardware,
         'matched_servers': matched_servers,
-        'mem_free_sum': mem_free_sum,
-        'mem_free_avg': mem_free_sum / mem_free_count,
-        'mem_total_sum': mem_total_sum,
-        'mem_total_avg': mem_total_sum / mem_total_count,
-        'disk_free_sum': disk_free_sum,
-        'disk_free_avg': disk_free_sum / disk_free_count,
-        'cpu_aggregate': cpu_aggregate,
-        'io_aggregate': io_aggregate,
         'understood': understood,
         'error': None
     })
