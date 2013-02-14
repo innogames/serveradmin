28d3f3eac149c3b91c6bcab15dff792b9d90c34a
Henning Pridöhl <henning@hprid.de>
2013-02-14 15:08:28 +0100

    adminapi: Add parameterless authorization in adminapi using .adminapirc

--- adminapi/__init__.py
+++ adminapi/__init__.py
@@ -1,4 +1,5 @@
 from adminapi.request import PermissionDenied
+from adminapi.cmdline.utils import get_auth_token
 
 BASE_URL = 'http://serveradmin.innogames.de/api'
 
@@ -6,5 +7,9 @@ _api_settings = {
     'auth_token': ''
 }
 
-def auth(auth_token):
+def auth(auth_token=None):
+    if auth_token is None:
+        auth_token = get_auth_token()
+        if not auth_token:
+            raise Exception('No auth token found')
     _api_settings['auth_token'] = auth_token
