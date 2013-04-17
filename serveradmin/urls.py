d398434284349d0cc3d7f29688f698e60b67607f
Henning Pridöhl <henning@hprid.de>
2013-04-17 16:41:53 +0200

    Add /media serving in debug mode

--- serveradmin/urls.py
+++ serveradmin/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.shortcuts import redirect
+from django.conf.urls.static import static
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -20,3 +22,7 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^comments/', include('comments.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
