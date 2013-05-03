23d38cfd258d8a84dedc318576888150d96a350c
Henning Pridöhl <henning@hprid.de>
2013-05-03 16:10:55 +0200

    adminapi: Don't add interactive file as module

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Henning Pridoehl',
     author_email='henning.pridoehl@innogames.de',
     packages=['adminapi', 'adminapi.dataset', 'adminapi.utils', 'adminapi.api',
-              'adminapi.cmdline', 'adminapi.interactive'],
+              'adminapi.cmdline'],
     version='0.35',
     long_description=('Admin remote API for querying servers and making'
                       'API requests')
