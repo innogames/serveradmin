f6d0e228928097b0949090dc4ab0ab5d1825836f
Henning Pridöhl <henning@hprid.de>
2013-05-27 20:37:21 +0200

    serverdb: Add two methods for attribute info to API

--- serveradmin/serverdb/api.py
+++ serveradmin/serverdb/api.py
@@ -1,5 +1,6 @@
 from serveradmin.api.decorators import api_function
 from serveradmin.serverdb.models import ServerType, Segment
+from serveradmin.dataset.base import lookups
 
 @api_function(group='dataset')
 def get_servertypes():
@@ -10,3 +11,37 @@ def get_servertypes():
 def get_segments():
     """Returns a list of available segments"""
     return [seg.segment for seg in Segment.objects.all()]
+
+@api_function(group='dataset')
+def get_attribute_info(servertype_name, attr_name):
+    """Returns default value for an attribute"""
+    try:
+        return _get_stype_attr_dict(servertype_name, attr_name)
+    except KeyError:
+        raise ValueError('Invalid servertype/attr combination: {0}/{1}'.format(
+                servertype_name, attr_name))
+
+@api_function(group='dataset')
+def get_servertype_attributes_info(servertype_name):
+    """Returns the attribute info for each attribute on a servertype"""
+    result = {}
+
+    try:
+        stype =  lookups.stype_names[servertype_name]
+    except KeyError:
+        raise ValueError('Invalid servertype: {0}'.format(servertype_name))
+
+    for attribute in stype.attributes:
+        result[attribute.name] = _get_stype_attr_dict(
+                servertype_name, attribute.name)
+
+    return result
+
+def _get_stype_attr_dict(servertype_name, attr_name):
+    index = (servertype_name, attr_name)
+    stype = lookups.stype_attrs[index]
+    return {
+        'default': stype.default,
+        'required': stype.required,
+        'regexp': stype.regexp.pattern if stype.regexp else None
+    }
