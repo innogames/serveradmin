3ed76673d1e3972838ea0fd9dad6b2fd2b434fd4
Henning Pridöhl <henning@hprid.de>
2013-06-07 17:03:28 +0200

    Add warning logo for secondary serveradmin

---
+++ serveradmin/common/templatetags/common.py
@@ -0,0 +1,11 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+@register.simple_tag
+def logo():
+    if getattr(settings, 'IS_SECONDARY', False):
+        return settings.STATIC_URL + 'logo_innogames_bigbulb_120_warn.png'
+    else:
+        return settings.STATIC_URL + 'logo_innogames_bigbulb_120.png'
