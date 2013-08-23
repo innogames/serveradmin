dad5846230168d4198677d51feee1bdf4b633262
Henning Pridöhl <henning@hprid.de>
2013-08-23 11:15:57 +0200

    adminapi: Bump version to 0.44

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='henning.pridoehl@innogames.de',
     packages=['adminapi', 'adminapi.dataset', 'adminapi.utils', 'adminapi.api',
               'adminapi.cmdline'],
-    version='0.43',
+    version='0.44',
     long_description=('Admin remote API for querying servers and making'
                       'API requests')
 )
