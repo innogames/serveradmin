5551a40882368ec3f8605057521c66810a6a689a
Henning Pridöhl <henning@hprid.de>
2014-03-28 11:21:38 +0100

    serverdb: Delete cache for addvalidations command

--- serveradmin/serverdb/management/commands/addvalidations.py
+++ serveradmin/serverdb/management/commands/addvalidations.py
@@ -1,4 +1,5 @@
 from django.core.management.base import NoArgsCommand
+from django.core.cache import cache
 from serveradmin.serverdb.models import Attribute, ServerTypeAttributes
 
 class Command(NoArgsCommand):
@@ -8,6 +9,7 @@ class Command(NoArgsCommand):
         ssh_regexp = r'^[a-z][a-z0-9._-]{1,30}:[a-z][a-z0-9._-]{1,30}$'
         self.add_validation('ssh_users', ssh_regexp)
         self.add_validation('ssh_groups', ssh_regexp)
+        cache.delete('dataset_lookups_version')
 
     def add_validation(self, attr_name, regex):
         attr = Attribute.objects.get(name=attr_name)
