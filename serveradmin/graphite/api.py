c45f36777c03cc9bf7509e95ff1cbaaf96dc34e7
Emre Hasegeli <emre.hasegeli@innogames.com>
2015-08-12 17:54:45 +0200

    Add API method to get Graphite NumericCache

---
+++ serveradmin/graphite/api.py
@@ -0,0 +1,14 @@
+from serveradmin.api.decorators import api_function
+from serveradmin.graphite.models import NumericCache
+
+@api_function(group='graphite')
+def get_numeric_cache(hostname):
+    """Get all the numeric cache entries we have for an hostname
+    """
+
+    return [{
+            'collection': str(n.template.collection),
+            'template': str(n.template),
+            'value': n.value,
+            'last_modified': n.last_modified,
+        } for n in NumericCache.objects.filter(hostname=hostname)]
