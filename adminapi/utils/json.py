4800011f7e963b2e06076460b48a5852fc19e4bd
Emre Hasegeli <emre.hasegeli@innogames.com>
2016-01-24 12:15:45 +0100

    Catch all types on json_encode_extra()

--- adminapi/utils/json.py
+++ adminapi/utils/json.py
@@ -1,8 +1,6 @@
 from time import mktime
 from datetime import datetime, date, timedelta
 
-from adminapi.utils import IP, IPv6, Network
-
 def json_encode_extra(obj):
 
     # Proxied sets are used by MultiAttr
@@ -12,19 +10,10 @@ def json_encode_extra(obj):
     if isinstance(obj, set):
         return list(obj)
 
-    if isinstance(obj, IP):
-        return obj.ip
-
-    if isinstance(obj, IPv6):
-        return obj.as_ip()
-
-    if isinstance(obj, Network):
-        return [obj.min_ip, obj.max_ip]
-
     if isinstance(obj, (datetime, date)):
         return int(mktime(obj.timetuple()))
 
     if isinstance(obj, timedelta):
         return obj.seconds + obj.days * 24 * 3600
 
-    raise TypeError('Type ' + str(type(obj)) + ' is not known')
+    return str(obj)
