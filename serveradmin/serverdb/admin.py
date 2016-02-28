c660ef2aeeb24ed382ccde7e1a38f7923afcd877
Emre Hasegeli <emre.hasegeli@innogames.com>
2016-02-28 12:25:27 +0100

    Add all models of serverdb to admin

--- serveradmin/serverdb/admin.py
+++ serveradmin/serverdb/admin.py
@@ -1,18 +1,41 @@
 from django.contrib import admin
 
 from serveradmin.serverdb.models import (
-        Project,
-        ServerType,
-        Attribute,
-        Segment,
-        ChangeDelete,
+    Project,
+    ServerType,
+    Attribute,
+    ServerTypeAttribute,
+    Segment,
+    ServerObject,
+    ServerHostnameAttribute,
+    ServerStringAttribute,
+    ChangeDelete,
+)
+
+class ServerTypeAttributeInline(admin.TabularInline):
+    model = ServerTypeAttribute
+
+class ServerTypeAdmin(admin.ModelAdmin):
+    inlines = (
+        ServerTypeAttributeInline,
     )
 
-class AttributeAdmin(admin.ModelAdmin):
-    fields = ('hovertext', 'help_link', 'group', 'readonly')
+class ServerHostnameAttributeInline(admin.TabularInline):
+    model = ServerHostnameAttribute
+    fk_name = 'server'
+
+class ServerStringAttributeInline(admin.TabularInline):
+    model = ServerStringAttribute
+
+class ServerObjectAdmin(admin.ModelAdmin):
+    inlines = (
+        ServerHostnameAttributeInline,
+        ServerStringAttributeInline,
+    )
 
 admin.site.register(Project)
-admin.site.register(ServerType)
-admin.site.register(Attribute, AttributeAdmin)
+admin.site.register(ServerType, ServerTypeAdmin)
+admin.site.register(Attribute)
 admin.site.register(Segment)
+admin.site.register(ServerObject, ServerObjectAdmin)
 admin.site.register(ChangeDelete)
