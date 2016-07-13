57218dc769cdfb6e7f78a2ec9c44bf1979704898
Emre Hasegeli <emre.hasegeli@innogames.com>
2016-07-13 12:19:33 +0200

    api: Don't search in error messages

--- serveradmin/api/middleware.py
+++ serveradmin/api/middleware.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from importlib import import_module
 
+
 class ApiMiddleware(object):
     """Load all api functions which reside in APPNAME.api module."""
 
@@ -8,7 +9,5 @@ class ApiMiddleware(object):
         for app in settings.INSTALLED_APPS:
             try:
                 import_module(app + '.api')
-            except ImportError as e:
-                if not 'No module named' in e.message:
-                    raise
+            except ImportError:
                 pass
