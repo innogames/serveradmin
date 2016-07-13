90c3e20bcf419eb480fc9a8026561ab015666685
Emre Hasegeli <emre.hasegeli@innogames.com>
2016-07-13 10:54:09 +0200

    hooks: Improve coding style

--- serveradmin/hooks/middleware.py
+++ serveradmin/hooks/middleware.py
@@ -1,14 +1,15 @@
 from django.conf import settings
 from importlib import import_module
 
+
 class HooksMiddleware(object):
-    """Load all hooks which reside in APPNAME.hooks module."""
+    """Load all hooks which reside in APPNAME.hooks module"""
 
     def __init__(self):
         for app in settings.INSTALLED_APPS:
             try:
                 import_module(app + '.hooks')
-            except ImportError as e:
-                if not 'No module named' in e.message:
+            except ImportError as error:
+                if 'No module named' not in error.message:
                     raise
                 pass
