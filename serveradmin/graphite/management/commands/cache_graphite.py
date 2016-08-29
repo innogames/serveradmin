c4db17df1cd471d76907313b8908bb52d0ba6857
Emre Hasegeli <emre.hasegeli@innogames.com>
2016-08-29 14:03:40 +0200

    Improve style of cache_graphite.py

--- serveradmin/graphite/management/commands/cache_graphite.py
+++ serveradmin/graphite/management/commands/cache_graphite.py
@@ -1,19 +1,23 @@
-import urllib2, json, time
+import urllib2
+import json
+import time
 from PIL import Image
 from io import BytesIO
 
 from django.core.management.base import NoArgsCommand
-from django.core.cache import cache
 from django.conf import settings
 from django.db import transaction
 
 import django_urlauth.utils
-from serveradmin.graphite.models import Collection, NumericCache, AttributeFormatter
+from serveradmin.graphite.models import (
+    Collection,
+    NumericCache,
+    AttributeFormatter,
+)
+
 
 class Command(NoArgsCommand):
-    """Generate sprites from the overview graphics
-    """
-
+    """Generate sprites from the overview graphics"""
     help = __doc__
 
     def handle_noargs(self, **kwargs):
@@ -30,17 +34,17 @@ class Command(NoArgsCommand):
                     done_servers.add(server)
 
     def generate_sprite(self, collection, server):
-        """Generates sprites for the given server using the given collection
-        """
+        """Generates sprites for the given server using the given collection"""
 
-        table = collection.graph_table(server,
-                                custom_params=settings.GRAPHITE_SPRITE_PARAMS)
+        table = collection.graph_table(
+            server, custom_params=settings.GRAPHITE_SPRITE_PARAMS
+        )
         graphs = [v2 for k1, v1 in table for k2, v2 in v1]
         sprite_width = (len(graphs) * settings.GRAPHITE_SPRITE_WIDTH +
                         (len(graphs) - 1) * settings.GRAPHITE_SPRITE_SPACING)
-        spriteimg = Image.new('RGB', (sprite_width,
-                                      settings.GRAPHITE_SPRITE_HEIGHT),
-                                      (255,) * 3)
+        spriteimg = Image.new(
+            'RGB', (sprite_width, settings.GRAPHITE_SPRITE_HEIGHT), (255,) * 3
+        )
         offset = 0
 
         for graph in graphs:
@@ -56,7 +60,6 @@ class Command(NoArgsCommand):
         spriteimg.save(settings.GRAPHITE_SPRITE_PATH + '/' +
                        server['hostname'] + '.png')
 
-
     def cache_numeric_values(self, collection, server):
         """Generates sprites for the given server using the given collection
         """
@@ -69,7 +72,10 @@ class Command(NoArgsCommand):
                 try:
                     value = json.loads(response)[0]['datapoints'][0][0]
                 except IndexError:
-                    print('Warning: Graphite response couldn\'t be parsed ' + response)
+                    print(
+                        "Warning: Graphite response couldn't be parsed: {0}"
+                        .format(response)
+                    )
                 else:
 
                     # Django can be setted up to encapsulate thing
@@ -89,8 +95,12 @@ class Command(NoArgsCommand):
         """Make a GET request to Graphite with the given params
         """
 
-        token = django_urlauth.utils.new_token('serveradmin', settings.GRAPHITE_SECRET)
-        url = settings.GRAPHITE_URL + '/render?__auth_token=' + token + '&' + params
+        token = django_urlauth.utils.new_token(
+            'serveradmin', settings.GRAPHITE_SECRET
+        )
+        url = '{0}/render?__auth_token={1}&{2}'.format(
+            settings.GRAPHITE_URL, token, params
+        )
 
         opener = urllib2.build_opener()
         start = time.time()
@@ -101,4 +111,7 @@ class Command(NoArgsCommand):
         finally:
             end = time.time()
             if end - start > 10:
-                print('Warning: Graphite request took ' + str(end - start) + ' seconds to ' + url)
+                print(
+                    'Warning: Graphite request to {0} took {1} seconds'
+                    .format(url, end - start)
+                )
