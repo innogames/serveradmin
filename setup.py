c7b88f6d5ddbf14844b25ee364b96732f9121541
Emre Hasegeli <emre.hasegeli@innogames.com>
2016-09-19 11:55:42 +0200

    adminapi: Bump version to 0.70

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         'adminapi.api',
         'adminapi.cmdline',
     ),
-    version='0.69',
+    version='0.70',
     long_description=(
         'Admin remote API for querying servers and making API requests'
     ),
