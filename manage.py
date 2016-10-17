b987527a578556d3f663f4c494f1b6fe52398706
Emre Hasegeli <emre.hasegeli@innogames.com>
2016-10-17 15:17:14 +0200

    Use python3 binary on manage.py shebang

--- manage.py
+++ manage.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "serveradmin.settings")
+from django.core.management import execute_from_command_line
 
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'serveradmin.settings')
+execute_from_command_line(sys.argv)
