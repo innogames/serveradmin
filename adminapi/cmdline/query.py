b64a4a2694ce856dc61f4ddfdb932f169484580d
Emre Hasegeli <emre.hasegeli@innogames.com>
2016-10-24 18:18:57 +0200

    Remove --token argument of query.py

--- adminapi/cmdline/query.py
+++ adminapi/cmdline/query.py
@@ -7,13 +7,11 @@ from optparse import OptionParser
 from adminapi.dataset import query, filters
 from adminapi.utils import format_obj
 from adminapi.utils.parse import parse_query
-from adminapi.cmdline.utils import get_auth_token
 
 
 def main():
     opt_parser = OptionParser()
     opt_parser.add_option('-a', '--attr', dest='attrs', action='append')
-    opt_parser.add_option('-t', '--token', dest='token')
     opt_parser.add_option('-n', '--null', dest='null_value', default='-')
     opt_parser.add_option('-e', '--export', action='store_true')
     opt_parser.add_option('-o', '--orderby', dest='orderby', action='append')
@@ -23,16 +21,6 @@ def main():
 
     if len(args) < 1:
         print('You have to supply a search term.', file=sys.stderr)
-        sys.exit(1)
-
-    if options.token:
-        auth_token = options.token
-    else:
-        auth_token = get_auth_token()
-
-    if auth_token is None:
-        print('No auth token found.', file=sys.stderr)
-        print('Use -t auth_token or create ~/.adminapirc', file=sys.stderr)
         sys.exit(1)
 
     attrs = options.attrs if options.attrs else ('hostname', )
