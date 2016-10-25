52e35322588db6ac2224408681b20cef4ab2136d
Oleg Obleukhov <oleg.obleukhov@innogames.com>
2016-10-25 10:37:01 +0200

    Cosmetic fixes

--- adminapi/cmdline/update.py
+++ adminapi/cmdline/update.py
@@ -3,46 +3,47 @@ from __future__ import print_function
 
 import sys, ipaddress, argparse
 from distutils.util import strtobool
-
 from adminapi.dataset import query, filters, base
 from adminapi.utils import format_obj
 from adminapi.utils.parse import parse_query
 from adminapi.cmdline.utils import get_auth_token
 
+
 def main():
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('hostname', help='Host on which changes need to be performed in serveradmin')
-    parser.add_argument('attrs', nargs='+', help='')
-    parser.add_argument('-o', '--overwrite', dest='overwrite', action='store_true', help='Overwrite multi attributes')
+    parser.add_argument('hostname', help = 'Host on which changes need to be performed in serveradmin')
+    parser.add_argument('attrs', nargs = '+', help = '')
+    parser.add_argument('-o', '--overwrite', dest = 'overwrite', action = 'store_true', help = 'Overwrite multi attributes')
     args=parser.parse_args()
 
     if not args.hostname:
-        print('You need to pass a hostname', file=sys.stderr)
+        print('You need to pass a hostname', file = sys.stderr)
         sys.exit(1)
 
     if not update(args.hostname, args.attrs, args.overwrite):
         sys.exit(1)
 
+
 def update(hostname, attrs, overwrite):
-    host = query(hostname=hostname).get()
+    host = query(hostname = hostname).get()
     for attr_value in attrs:
         if len(attr_value.split('=')) != 2:
             print('You need to pass an attribute=value', file=sys.stderr)
             return False
-        else:
-            attr, value = attr_value.split('=')
-            if type(host[attr]) is base.MultiAttr:
-                if overwrite:
-                    host[attr] = set([value])
-                else:
-                    host[attr].add(value)
-            elif type(host[attr]) is bool:
-                host[attr] = strtobool(value)
-            elif type(host[attr]) is int:
-                host[attr] = int(value)
+        attr, value = attr_value.split('=')
+        if type(host[attr]) is base.MultiAttr:
+            if overwrite:
+                host[attr] = set([value])
             else:
+                host[attr].add(value)
+        elif type(host[attr]) is bool:
+            host[attr] = strtobool(value)
+        elif type(host[attr]) is int:
+            host[attr] = int(value)
+        else:
                 host[attr] = value
     host.commit()
+
 
 if __name__ == '__main__':
     main()
