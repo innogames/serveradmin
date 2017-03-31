1da71c0edf8eccdbebedec7436f394bb73ac8ab2
Emre Hasegeli <emre.hasegeli@innogames.com>
2017-03-31 12:24:16 +0200

    Load URLs dynamically

--- serveradmin/urls.py
+++ serveradmin/urls.py
@@ -1,5 +1,7 @@
+from importlib import import_module
+
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.models import update_last_login
@@ -11,24 +13,24 @@ user_logged_in.disconnect(update_last_login)
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
-    url(r'^$', lambda req: redirect('servershell_index'),  name='home'),
-    url(r'^servershell/', include('serveradmin.servershell.urls')),
-    url(r'^serverdb/', include('serveradmin.serverdb.urls')),
-    url(r'^apps/', include('serveradmin.apps.urls')),
-    url(r'^api/', include('serveradmin.api.urls')),
-    url(r'^documentation/', include('serveradmin.docs.urls')),
-    url(r'^colocation/', include('serveradmin.colo.urls')),
-    url(r'^loginapi/', include('igrestlogin.urls')),
+urlpatterns = [
+    url(r'^$', lambda req: redirect('servershell_index'), name='home'),
     url(r'^logout', 'django.contrib.auth.views.logout_then_login',
         name='logout'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^graphite/', include('serveradmin.graphite.urls')),
-    url(r'^resources/', include('serveradmin.resources.urls')),
     url(r'^failoverlogin$', 'serveradmin.common.views.failoverlogin'),
     url(r'^check$', 'serveradmin.common.views.check'),
-)
+]
+
+for app in settings.INSTALLED_APPS:
+    try:
+        module = import_module(app + '.urls')
+    except ImportError:
+        continue
+
+    urlpatterns.append(
+        url(r'^{}/'.format(app.rsplit('.', 1)[-1]), include(module))
+    )
 
 if settings.DEBUG:
     urlpatterns += static(
