1d042afd8739c02b4388ac9de6768626ba5ce379
Emre Hasegeli <emre.hasegeli@innogames.com>
2017-07-17 18:15:53 +0200

    apps: Remove urls

--- serveradmin/apps/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns(
-    'serveradmin.apps.views',
-    url(r'^exc/(\w+)$', 'request_exception', name='apps_request_exception'),
-    url(r'^exc_filled$', 'exception_filled', name='apps_exception_filled'),
-)
