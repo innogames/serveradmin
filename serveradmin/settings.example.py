1fba37e26b85536de1abda96455acf49126f1e2d
Emre Hasegeli <emre.hasegeli@innogames.com>
2017-08-21 17:11:24 +0200

    Update language settings

--- serveradmin/settings.example.py
+++ serveradmin/settings.example.py
@@ -35,17 +35,17 @@ TIME_ZONE = 'Europe/Berlin'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'de-de'
+LANGUAGE_CODE = 'en_DK'
 
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = False
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale.
-USE_L10N = True
+USE_L10N = False
 
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
