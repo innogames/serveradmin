39a236829aa1b683d17513ac9327ebf6fa229434
Emre Hasegeli <emre.hasegeli@innogames.com>
2017-12-12 11:25:39 +0100

    adminapi: Provide backwards compatibility only for existing filters

--- adminapi/dataset/filters.py
+++ adminapi/dataset/filters.py
@@ -1,2 +1,16 @@
 # XXX Deprecated
-from adminapi.filters import *  # NOQA F401 F403
+from adminapi.filters import (  # NOQA F401
+    BaseFilter,
+    ExactMatch,
+    Regexp,
+    Comparison,
+    Any,
+    Or,
+    And,
+    Not,
+    Overlap,
+    Startswith,
+    InsideNetwork,
+    Empty,
+    filter_classes,
+)
