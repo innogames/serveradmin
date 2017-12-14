3707aab5598e440b903f7dfd056fc73cfff29491
Emre Hasegeli <emre.hasegeli@innogames.com>
2017-12-14 14:40:39 +0100

    Log API requests

--- serveradmin/api/decorators.py
+++ serveradmin/api/decorators.py
@@ -1,5 +1,6 @@
-import time
+from time import time
 from functools import update_wrapper
+from logging import getLogger
 try:
     import simplejson as json
 except ImportError:
@@ -17,17 +18,25 @@ from adminapi.request import calc_security_token, json_encode_extra
 from serveradmin.apps.models import Application
 from serveradmin.api import AVAILABLE_API_FUNCTIONS
 
+logger = getLogger('serveradmin')
+
 
 def api_view(view):
     @csrf_exempt
     def _wrapper(request):
+        logger.debug('api: Start processing request: {} {}'.format(
+            request.scheme, request.path
+        ))
+
         problem = check_authentication_headers(request)
         if problem:
+            logger.error('api: Bad request: ' + problem)
             return HttpResponseBadRequest(problem, content_type='text/plain')
 
         app_id = request.META['HTTP_X_APPLICATION']
-        security_token = request.META['HTTP_X_SECURITYTOKEN']
+        token = request.META['HTTP_X_SECURITYTOKEN']
         timestamp = int(request.META['HTTP_X_TIMESTAMP'])
+        now = time()
         body = request.body.decode('utf8')
 
         try:
@@ -35,14 +44,23 @@ def api_view(view):
         except Application.DoesNotExist:
             problem = 'No such application'
         else:
-            problem = authenticate_app(app, security_token, timestamp, body)
+            if timestamp + 300 < now:
+                problem = 'Expired security token'
+            else:
+                problem = authenticate_app(app, token, timestamp, body)
         if problem:
+            logger.error('api: Forbidden: ' + problem)
             return HttpResponseForbidden(problem, content_type='text/plain')
 
         return_value = view(request, app, json.loads(body))
         if getattr(view, 'encode_json', True):
             return_value = json.dumps(return_value, default=json_encode_extra)
 
+        logger.info('api: Call: ' + (', '.join([
+            'Method: {}'.format(view.__name__),
+            'Application: {}'.format(app),
+            'Time elapsed: {:.3f}s'.format(time() - now),
+        ])))
         return HttpResponse(return_value, content_type='application/x-json')
 
     return update_wrapper(_wrapper, view)
@@ -58,13 +76,10 @@ def check_authentication_headers(request):
         return 'Malformatted header "HTTP_X_TIMESTAMP"'
 
 
-def authenticate_app(app, security_token, timestamp, body):
+def authenticate_app(app, token, timestamp, body):
     real_token = calc_security_token(app.auth_token, timestamp, body)
-    if not constant_time_compare(real_token, security_token):
+    if not constant_time_compare(real_token, token):
         return 'Invalid security token'
-
-    if timestamp + 300 < time.time():
-        return 'Expired security token'
 
     if app.owner is not None and not app.owner.is_active:
         return 'Inactive user'
