2711e07e6b845a170d12ddea89dea2f9bbb13b14
Emre Hasegeli <emre.hasegeli@innogames.com>
2017-12-18 15:41:02 +0100

    Remove deprecated adminapi modules

--- adminapi/dataset/base.py
+++
@@ -1,7 +0,0 @@
-"""Serveradmin - adminapi
-
-Copyright (c) 2019 InnoGames GmbH
-"""
-
-# XXX Deprecated
-from adminapi.dataset import DatasetError, MultiAttr    # NOQA: F401
--- adminapi/utils/parse.py
+++
@@ -1,7 +0,0 @@
-"""Serveradmin - adminapi
-
-Copyright (c) 2019 InnoGames GmbH
-"""
-
-# XXX Deprecated
-from adminapi.parse import parse_query      # NOQA F401
