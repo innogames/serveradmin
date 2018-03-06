c1b65e5046f29de06f7f05cd3935304464c4f0f5
Emre Hasegeli <emre.hasegeli@innogames.com>
2018-03-06 17:57:55 +0100

    Simplify access control groups

--- serveradmin/access_control/migrations/0001_initial.py
+++ serveradmin/access_control/migrations/0001_initial.py
@@ -9,21 +9,18 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('apps', '__first__'),
+        ('apps', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='AccessControlGroup',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
                 ('name', models.CharField(max_length=80, unique=True)),
-                ('create_server_query', models.CharField(max_length=1000)),
-                ('edit_server_query', models.CharField(max_length=1000)),
-                ('commit_server_query', models.CharField(max_length=1000)),
-                ('delete_server_query', models.CharField(max_length=1000)),
-                ('applications', models.ManyToManyField(blank=True, to='apps.Application', related_name='access_control_groups')),
-                ('members', models.ManyToManyField(blank=True, to=settings.AUTH_USER_MODEL, related_name='access_control_groups')),
+                ('query', models.CharField(max_length=1000)),
+                ('applications', models.ManyToManyField(related_name='access_control_groups', blank=True, to='apps.Application')),
+                ('members', models.ManyToManyField(related_name='access_control_groups', blank=True, to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'db_table': 'access_control_group',
