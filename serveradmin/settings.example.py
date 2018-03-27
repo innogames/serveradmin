c8f01cf1f6344f7320a4259d087046cfe840b952
Emre Hasegeli <emre.hasegeli@innogames.com>
2018-03-27 18:06:44 +0200

    Update context_processors namespace on settings
    They are moved to another namespace on Django 1.8.

--- serveradmin/settings.example.py
+++ serveradmin/settings.example.py
@@ -99,11 +99,11 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.contrib.auth.context_processors.auth',
-                'django.core.context_processors.debug',
-                'django.core.context_processors.media',
-                'django.core.context_processors.static',
-                'django.core.context_processors.tz',
-                'django.core.context_processors.request',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.template.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
                 'serveradmin.common.context_processors.base',
             ],
