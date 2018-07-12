7ead4e12997a6d60cdc10eb4a32d28945fbaa829
Emre Hasegeli <emre.hasegeli@innogames.com>
2018-07-12 11:19:27 +0200

    Add note to run.py

--- run.py
+++ run.py
@@ -3,6 +3,9 @@
 
 Copyright (c) 2018 InnoGames GmbH
 """
+# NOTE: This binary is provided for convenience.  It is nice to be able to run
+# the checks from the repository.  This is not included on the releases.  We
+# are using entry_points mechanism on the setup.py.
 
 from adminapi.cli import main
 
