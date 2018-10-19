95cfc8484416877e2f3b4578c833363d067e339c
Patrick Meyer <patrick.meyer@innogames.com>
2018-10-19 11:51:55 +0200

    Add serveradmin modules to setup.py
    Update setup.py
    
    Co-Authored-By: HerrSpace <git@the-space.agency>

--- setup.py
+++ setup.py
@@ -1,26 +1,52 @@
 #!/usr/bin/env python
-"""Serveradmin - adminapi - Setup
+"""Serveradmin and adminapi setup
 
 Copyright (c) 2018 InnoGames GmbH
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
+from serveradmin import VERSION as SERVERADMIN_VERSION
 
-setup(
-    name='adminapi',
-    url='https://github.com/innogames/serveradmin',
-    author='InnoGames System Administration',
-    author_email='it@innogames.com',
-    packages=[
-        'adminapi',
-        'adminapi.dataset',     # XXX Deprecated
-        'adminapi.utils',       # XXX Deprecated
-    ],
-    version='1.3',
-    long_description='Serveradmin client library',
-    entry_points={
-        'console_scripts': [
-            'adminapi=adminapi.cli:main',
-        ],
-    },
-)
+
+if __name__ == '__main__':
+    setup(
+        version='.'.join(map(str, SERVERADMIN_VERSION)),
+        name='adminapi',
+        description='Serveradmin module',
+        url='https://github.com/innogames/serveradmin',
+        packages=find_packages(exclude=["serveradmin_*"]),
+        package_data={
+            'serveradmin.api': ['templates/api/*'],
+            'serveradmin.apps': ['templates/apps/*'],
+            'serveradmin.common': [
+                'static/*',
+                'static/common/*',
+                'static/flot/*',
+                'static/images/*',
+                'templates/*',
+                'templates/igrestlogin/*',
+            ],
+            'serveradmin.graphite': [
+                'static/graphite/*',
+                'templates/graphite/*',
+            ],
+            'serveradmin.resources': ['templates/resources/*'],
+            'serveradmin.serverdb': [
+                'static/*'
+                'templates/serverdb/*',
+            ],
+            'serveradmin.servershell': [
+                'static/*',
+                'static/servershell/*',
+                'templates/servershell/*',
+            ],
+        },
+        entry_points={
+            'console_scripts': [
+                'serveradmin=serveradmin.__main__:main',
+                'adminapi=adminapi.__main__:main',
+            ],
+        },
+        author='InnoGames System Administration',
+        author_email='it@innogames.com',
+    )
