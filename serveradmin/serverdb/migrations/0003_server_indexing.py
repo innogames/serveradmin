963274a77ccc09ea43634224a53064dc5ffea23c
Patrick Meyer <patrick.meyer@innogames.com>
2019-03-12 12:33:20 +0100

    Improve uniq ip constraints
    As suggested by emre in:
    https://github.com/innogames/serveradmin/pull/60#discussion_r263806849

--- serveradmin/serverdb/migrations/0003_server_indexing.py
+++ serveradmin/serverdb/migrations/0003_server_indexing.py
@@ -15,16 +15,44 @@ class Migration(migrations.Migration):
         # Ensure objects within the same servertype have a unique intern_ip.
         migrations.RunSQL('CREATE EXTENSION IF NOT EXISTS btree_gist'),
         migrations.RunSQL(
-            'ALTER TABLE server '
-            'ADD CONSTRAINT server_inter_ip_exclude '
-            '   EXCLUDE USING gist ('
-            '       intern_ip inet_ops WITH &&,'
-            '       servertype_id WITH ='
-            '   )'
-            # At InnoGames we have a servertype loadbalancer which is the only
-            # servertype allowed to have multiple objects sharing the same
-            # intern_ip.  We use this to forward different ports to different
-            # hosts as well as for multi datacenter virtual IPs.
-            "   WHERE (servertype_id != 'loadbalancer')"
+            # We can't directly use the ip_addr_type from the servertype table
+            # in constraints for the server table. Hence we need this function.
+            "CREATE FUNCTION get_ip_addr_type(text) RETURNS text"
+            "    LANGUAGE sql STRICT IMMUTABLE"
+            "    AS 'SELECT ip_addr_type FROM servertype WHERE servertype_id = $1'; "
+            "ALTER TABLE server"
+            # Objects of servertypes with the ip_addr_type null, must not have
+            # an ip address set.
+            "    ADD CONSTRAINT server_inter_ip_check"
+            "    CHECK ((get_ip_addr_type(servertype_id) = 'null') = (intern_ip IS NULL)),"
+            # Objects of servertypes with the ip_addr_type host, must not have
+            # the same ip address on multiple objects.
+            "    ADD CONSTRAINT server_host_inter_ip_exclude"
+            "    EXCLUDE ("
+            "        intern_ip WITH ="
+            "    )"
+            "    WHERE (get_ip_addr_type(servertype_id) = 'host'),"
+            # Objects of servertypes with the ip_addr_type loadbalancer, are
+            # allowed to use the same ip address on multiple objects. This
+            # exlucdes ip addresses used on an object of a servertypes with the
+            # ip_addr_type host though.
+            # We use this to forward different ports to different hosts as well
+            # as for multi datacenter virtual IPs.
+            "    ADD CONSTRAINT server_host_loadbalancer_inter_ip_exclude"
+            "    EXCLUDE USING gist ("
+            "        intern_ip WITH =,"
+            "        servertype_id WITH !="
+            "    )"
+            "    WHERE (get_ip_addr_type(servertype_id) IN ('host', 'loadbalancer')),"
+            # Objects of servertypes with the ip_addr_type network, may not
+            # have overlapping ip networks within the same servertype.
+            # For instance a route_network may be inside a provider_network but
+            # a provider_network may not overlap with another provider_network.
+            "    ADD CONSTRAINT server_network_inter_ip_exclude"
+            "    EXCLUDE USING gist ("
+            "        intern_ip inet_ops WITH &&,"
+            "        servertype_id WITH ="
+            "    )"
+            "    WHERE (get_ip_addr_type(servertype_id) = 'network');"
         ),
     ]
