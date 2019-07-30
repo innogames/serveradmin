d9ea236eac11a573c6528c87812d3514c065256e
Patrick Meyer <patrick.meyer@innogames.com>
2019-07-30 17:09:58 +0200

    docs: Test docs build via travis ci

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,10 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -28,7 +27,12 @@ author = 'InnoGames GmbH'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
 ]
+intersphinx_mapping = {
+    "python": ('https://docs.python.org/3', None),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -38,6 +42,11 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# Warn about all references to unknown targets
+nitpicky = True
+
+# Enter at index.rst, old sphinx versions try contents.rst
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
