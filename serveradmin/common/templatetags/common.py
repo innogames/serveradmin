a8b6bd5787da8d47b2ce7ebfedfef284971652c2
Luiz Amaral <luiz.amaral@innogames.com>
2019-12-13 15:58:15 +0100

    Allow the logo filename to be customized via settings.py

--- serveradmin/common/templatetags/common.py
+++ serveradmin/common/templatetags/common.py
@@ -13,10 +13,7 @@ register = template.Library()
 
 @register.simple_tag
 def logo():
-    if getattr(settings, 'IS_SECONDARY', False):
-        return settings.STATIC_URL + 'logo_innogames_bigbulb_120_warn.png'
-    else:
-        return settings.STATIC_URL + 'logo_innogames_bigbulb_120.png'
+    return settings.STATIC_URL + settings.LOGO_FILENAME
 
 
 @register.filter
