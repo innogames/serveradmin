e849685356521bd806df1d92256b11768dd73b3a
Daniel Kröger <daniel.kroeger@innogames.com>
2020-01-08 15:48:16 +0100

    feature(common): spinner, messages and docs in base.html
    * Add spinner which can be used to indicate running actions in the background
    * Add documentation for twitter bootstrap in HTML
    * Add messages raised from backend back to base.html (lost in transition)
    * Fix STATIC_URL usage (extra slash)
    * Fix current menu entry in navigation not active

---
+++ serveradmin/common/static/js/serveradmin.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    // Mark the current page as active in navigation
+    let selector = 'nav .nav-link[href="' + window.location.pathname + '"]';
+    $(selector)[0].classList.add('active');
+
+    // This is our progress spinner we can call e.g. spinner.enable() from
+    // everywhere when ever we need it for example when doing long running
+    // ajax requests for the servershell search.
+    spinner = {
+        "enable": function () {
+            $('#spinner').show();
+        },
+        "disabled": function () {
+            $('#spinner').hide();
+        }
+    }
+});
