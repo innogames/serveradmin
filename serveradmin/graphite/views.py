0950bba24f126120fc2a4af89d6d270415033cc6
Daniel Kröger <daniel.kroeger@innogames.com>
2020-03-02 16:29:22 +0100

    fix(graphite): Unknown hostname or object_id throws 500
    Adding spaces or invalid hostnames or object_ids to the filter crashes
    the page with a 500. Lets show the user a message and try to still
    render the page.

--- serveradmin/graphite/views.py
+++ serveradmin/graphite/views.py
@@ -9,30 +9,36 @@ from urllib.request import (
     build_opener
 )
 
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http import (
     HttpResponse, HttpResponseBadRequest, HttpResponseServerError
 )
 from django.template.response import TemplateResponse
-from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import ensure_csrf_cookie
-from django.conf import settings
 
-from adminapi.dataset import DatasetError, MultiAttr
+from adminapi.dataset import MultiAttr
 from adminapi.filters import Any
+from serveradmin.dataset import Query
 from serveradmin.graphite.models import (
     GRAPHITE_ATTRIBUTE_ID,
     Collection,
-    format_attribute_value,
 )
-from serveradmin.dataset import Query
 
 
 @login_required     # NOQA: C901
 @ensure_csrf_cookie
 def graph_table(request):
-    """Graph table page"""
-    hostnames = [h for h in request.GET.getlist('hostname', []) if h]
-    object_ids = [o for o in request.GET.getlist('object_id', []) if o]
+    """Generate graph table page
+
+    :param request:
+    :return:
+    """
+
+    hostnames = [h.strip() for h in request.GET.getlist('hostname', []) if h]
+    object_ids = [o.strip() for o in request.GET.getlist('object_id', []) if o]
+
     if len(hostnames) == 0 and len(object_ids) == 0:
         return HttpResponseBadRequest('No hostname or object_id provided')
 
@@ -41,6 +47,12 @@ def graph_table(request):
                Query({'hostname': Any(*hostnames)}, None)}
     servers.update({s['hostname']: s for s in
                     Query({'object_id': Any(*object_ids)}, None)})
+
+    if len(servers) != len(hostnames) + len(object_ids):
+        messages.error(
+            request,
+            'One or more objects with hostname: {} or object_ids: {} does not '
+            'exist'.format(','.join(hostnames), ','.join(object_ids)))
 
     # Find the collections which are related with all of the hostnames.
     # If there are two collections with same match, use only the one which
@@ -69,8 +81,9 @@ def graph_table(request):
 
     # Prepare the graph tables for all hosts
     graph_tables = []
+    graph_table = []
     for hostname in servers.keys():
-        graph_table = []
+        graph_table.clear()
         if request.GET.get('action') == 'Submit':
             custom_params = request.GET.urlencode()
             for collection in collections:
@@ -83,19 +96,14 @@ def graph_table(request):
                 graph_table += collection.graph_table(servers[hostname])
         graph_tables.append(graph_table)
 
-    grafana_link_params = '?'
-    if len(hostname) > 1:
+    if len(servers) > 1:
         # Add hostname to the titles
         for order, hostname in enumerate(servers.keys()):
-            graph_tables[order] = [
-                (k + ' on ' + hostname, v) for k, v in graph_tables[order]
-            ]
-            grafana_link_params += 'var-SERVER={}&'.format(
-                format_attribute_value(hostname)
-            )
+            graph_tables[order] = [(k + ' on ' + hostname, v) for k, v in
+                                   graph_tables[order]]
 
         # Combine them
-        graph_table = []
+        graph_table.clear()
         for combined_tables in zip(*graph_tables):
             graph_table += list(combined_tables)
 
@@ -103,12 +111,9 @@ def graph_table(request):
         'hostnames': servers.keys(),
         'descriptions': descriptions,
         'graph_table': graph_table,
-        'is_ajax': request.is_ajax(),
-        'base_template': 'empty.html' if request.is_ajax() else 'base.html',
         'link': request.get_full_path(),
         'from': request.GET.get('from', '-24h'),
         'until': request.GET.get('until', 'now'),
-        'grafana_link': settings.GRAFANA_DASHBOARD + grafana_link_params
     })
 
 
