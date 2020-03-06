dec2c52429ca573e57d54e16198038f06badd829
Daniel Kröger <daniel.kroeger@innogames.com>
2020-03-06 10:29:31 +0100

    fix(servershell): Minor fixes for auto complete
    - Fix reverse attribtues not suggested for attr command
    - Fix spaces make attr command show all and don't filter
    - Add documentation and reformat code

--- serveradmin/servershell/static/js/servershell/autocomplete/command.js
+++ serveradmin/servershell/static/js/servershell/autocomplete/command.js
@@ -1,122 +1,171 @@
 $(document).ready(function() {
-    let get_attribute_ids = function(search_string, already_selected, exclude_multi=false, exclude_single=false) {
-        let attribute_ids = servershell.attributes;
+    /**
+     * Get filtered list of attribute ids
+     *
+     * Takes a search value and some more configuration and returns filtered
+     * list of attribute ids for the auto completion.
+     *
+     * @param search_for search string such as e.g. puppe
+     * @param exclude list of attributes already used
+     * @param exclude_multi exclude multi attributes
+     * @param exclude_single exclude single attributes
+     * @param exclude_reverse exclude reverse attributes
+     * @returns alphabetically sorted list of attribute ids
+     */
+    let get_attribute_ids = function (
+        search_for, exclude, exclude_multi = false,
+        exclude_single = false, exclude_reverse = false
+    ) {
+        let attrs = servershell.attributes;
 
-        if (search_string)
-            attribute_ids = attribute_ids.filter(
-                a => a.attribute_id.substring(0, a.attribute_id.length - 1).startsWith(search_string) && a.type !== 'reverse');
+        // TODO: Iterate only once over attributes and evaluate all filter
 
-       if (already_selected)
-           attribute_ids = attribute_ids.filter(a => !already_selected.includes(a.attribute_id));
+        if (search_for)
+            attrs = attrs.filter(a => a.attribute_id.startsWith(search_for));
+
+        if (exclude_reverse)
+            attrs = attrs.filter(a => a.type !== 'reverse');
+
+       if (exclude)
+           attrs = attrs.filter(a => !exclude.includes(a.attribute_id));
 
        if (exclude_multi)
-           attribute_ids = attribute_ids.filter(a => a.multi === false);
+           attrs = attrs.filter(a => a.multi === false);
 
        if (exclude_single)
-           attribute_ids = attribute_ids.filter(a => a.multi === true);
+           attrs = attrs.filter(a => a.multi === true);
 
-       return attribute_ids.map(a => a.attribute_id).sort();
+       return attrs.map(a => a.attribute_id).sort();
     };
 
-   $('#command').autocomplete({
-       delay: 100,
-       minLength: 0,
-       autoFocus: true,
-       source: function(request, response) {
-           let choices = [];
-           let arguments = request.term.split(' ', 2).map(v => v.trim());
+    let command_input = $('#command');
 
-           if (arguments.length === 1) {
-               let command = arguments.pop();
-               Object.keys(servershell.commands).sort().forEach(function(name) {
-                   if (name !== command && name.startsWith(command)) {
-                       choices.push({
-                           'label': name,
-                           'value': `${name} `,
-                       });
-                   }
-               });
-           }
-           else {
-               let command = arguments[0].trim();
-               let values = arguments[1].split(',').map(v => v.trim());
+    command_input.autocomplete({
+        delay: 100,
+        minLength: 0,
+        autoFocus: true,
+        source: function(request, response) {
+            let choices = [];
+            let arguments = request.term.split(' ');
 
-               if (command === 'goto') {
-                   [...Array(servershell.pages()).keys()].forEach(function (p) {
-                       let page = p + 1;
-                       if (page !== servershell.page()) {
-                           choices.push({
-                               'label': page,
-                               'value': `${command} ${page}`,
-                           });
-                       }
-                   })
-               }
-               if (command === 'perpage') {
-                   let user_input = values.pop();
-                   if (user_input === '') {
-                       [5, 10, 15, 20, 25, 50, 100].forEach(function (perpage) {
-                           choices.push({
-                               'label': perpage,
-                               'value': `${command} ${perpage}`,
-                           })
-                       });
-                   }
-               }
-               if (command === 'attr' || command === 'export') {
-                   let search_string = values.pop();
-                   let attribute_ids = get_attribute_ids(search_string, values);
-                   attribute_ids.slice(0, 25).forEach(function (attribute_id) {
-                       let values_string = values.length > 0 ? values.join(',') + ',' : '';
-                       choices.push({
-                           'label': `Attr: ${attribute_id}`,
-                           'value': `${command} ${values_string}${attribute_id}`,
-                       })
-                   });
-               }
-               if (command === 'orderby' || command === 'delattr' || command === 'history') {
-                   let search_string = values.shift();
-                   let attribute_ids = get_attribute_ids(search_string);
-                   attribute_ids.slice(0, 25).forEach(function(attribute_id) {
+            // Auto complete for all available commands
+            if (arguments.length <= 1) {
+                let command = arguments.length === 1 ? arguments.pop() : '';
+                let commands = servershell.commands;
+
+                Object.keys(commands).sort().forEach(function(name) {
+                    if (
+                        command === '' ||
+                        (name !== command && name.startsWith(command))
+                    ) {
+                        choices.push({
+                            'label': name,
+                            'value': `${name} `,
+                        });
+                    }
+                });
+            }
+            else {
+                // Auto complete for certain commands and its values
+                let command = arguments.shift().trim();
+                let values = arguments.join();
+
+                if (command === 'goto') {
+                    let pages = [...Array(servershell.pages()).keys()];
+                    pages.forEach(function (p) {
+                        let page = p + 1;
+                        if (page !== servershell.page()) {
+                            choices.push({
+                                'label': page,
+                                'value': `${command} ${page}`,
+                            });
+                        }
+                    })
+                }
+                if (command === 'perpage') {
+                    let page_size = values.trim();
+                    if (page_size === '' || isNaN(page_size))
+                        page_size = null;
+
+                    let pages = [5, 10, 15, 20, 25, 50, 100];
+                    pages.forEach(function (per_page) {
+                        per_page = per_page.toString();
+                        if (page_size === null || per_page.startsWith(page_size)) {
+                            choices.push({
+                                'label': per_page,
+                                'value': `${command} ${per_page}`,
+                            });
+                        }
+                    });
+                }
+                if (command === 'attr' || command === 'export') {
+                    values = values.split(',').map(v => v.trim());
+                    let search_string = values.pop();
+                    let attribute_ids = get_attribute_ids(search_string, values);
+
+                    attribute_ids.slice(0, 25).forEach(function (attribute_id) {
+                        let values_string = values.length > 0 ? values.join(',') + ',' : '';
+                        choices.push({
+                            'label': `Attr: ${attribute_id}`,
+                            'value': `${command} ${values_string}${attribute_id}`,
+                        })
+                    });
+                }
+                if (
+                    command === 'orderby' ||
+                    command === 'delattr' ||
+                    command === 'history'
+                ) {
+                    let attribute_ids = get_attribute_ids(
+                        values, [], false, false, command === 'delattr'
+                    );
+                    attribute_ids.slice(0, 25).forEach(function (attribute_id) {
                         choices.push({
                             'label': `Attr: ${attribute_id}`,
                             'value': `${command} ${attribute_id}`,
                         });
-                   });
-               }
-               if (command === 'setattr' || command === 'multiadd' || command === 'multidel') {
-                   let search_string = values.shift();
-                   if (!search_string.includes('=')) {
+                    });
+                }
+                if (
+                    command === 'setattr' ||
+                    command === 'multiadd' ||
+                    command === 'multidel'
+                ) {
+                    let search_string = values.split(' ').shift();
+                    if (!search_string.includes('=')) {
                         let attribute_ids = get_attribute_ids(
                             search_string, values,
                             command === 'setattr',
-                            command === 'multiadd' || command === 'multidel');
-                        attribute_ids.slice(0, 25).forEach(function(attribute_id) {
+                            command === 'multiadd' || command === 'multidel',
+                            true,
+                        );
+                        attribute_ids.slice(0, 25).forEach(function (attribute_id) {
                             choices.push({
                                 'label': `Attr: ${attribute_id}`,
                                 'value': `${command} ${attribute_id}=`,
                             });
                         });
-                   }
-               }
-           }
+                    }
+                }
+            }
 
-           response(choices);
-       }
-   })
-});
+            response(choices);
+        }
+    });
 
-$('#command').on('autocompleteselect', function(event, ui) {
-    if (ui.item.value.endsWith(' ')) {
-        let ac = function() {
-            $('#command').autocomplete('search')
-        };
-        // When triggering autocomplete right away nothing happens, with
-        // a small delay it works fine.
-        setTimeout(ac, 50);
-    }
-});
+    command_input.on('autocompleteselect', function(event, ui) {
+        let value = ui.item.value;
+        if (value === '' || value.endsWith(' ')) {
+            let ac = function() {
+                $('#command').autocomplete('search')
+            };
+            // When triggering autocomplete right away nothing happens, with
+            // a small delay it works fine.
+            setTimeout(ac, 50);
+        }
+    });
 
-$('#command').on('autocompleteclose', function () {
-    $('#command').focus();
+    command_input.on('autocompleteclose', function () {
+        $('#command').focus();
+    });
 });
