1e4d8c8d34ab0a80d2b25bb842c8b1d3b8dc5c0b
Daniel Kröger <daniel.kroeger@innogames.com>
2020-03-09 15:40:51 +0100

    fix(common): Wrong path for javascript plugins

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
                 'static/css/*',
                 'static/icons/*',
                 'static/js/*',
-                'static/plugins/*',
+                'static/js/plugins/*',
                 'templates/*',
                 'templates/igrestlogin/*',
             ],
