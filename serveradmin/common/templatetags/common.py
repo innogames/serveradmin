e59dcb18be107e41b860f5eab3642aafe4929efe
Daniel Kröger <daniel.kroeger@innogames.com>
2020-04-02 15:42:03 +0200

    task(common): Delete unused templatetags

--- serveradmin/common/templatetags/common.py
+++ serveradmin/common/templatetags/common.py
@@ -1,29 +1,16 @@
 """Serveradmin
 
-Copyright (c) 2019 InnoGames GmbH
+Copyright (c) 2020 InnoGames GmbH
 """
 
-import socket
-
 from django import template
-from django.conf import settings
 
 register = template.Library()
-
-
-@register.simple_tag
-def logo():
-    return settings.STATIC_URL + settings.LOGO_FILENAME
 
 
 @register.filter
 def dict_get(value, arg):
     return value.get(arg)
-
-
-@register.simple_tag
-def gethostname():
-    return socket.gethostname()
 
 
 @register.filter
