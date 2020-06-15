cea3f3eabef570137a9667e598e43f599d06b6c1
kofrezo <daniel.kroeger@kofrezo.io>
2020-06-15 20:24:13 +0200

    task(serverdb): Update urls for Django 2.x

--- serveradmin/serverdb/urls.py
+++ serveradmin/serverdb/urls.py
@@ -1,18 +1,15 @@
 """Serveradmin
 
-Copyright (c) 2019 InnoGames GmbH
+Copyright (c) 2020 InnoGames GmbH
 """
 
-from django.conf.urls import url
+from django.urls import path
 
 from serveradmin.serverdb.views import changes, restore_deleted, history
 
 urlpatterns = [
-    url(r'^changes$', changes, name='serverdb_changes'),
-    url(
-        r'^changes_restore/(\d+)$',
-        restore_deleted,
-        name='serverdb_restore_deleted',
-    ),
-    url(r'^history$', history, name='serverdb_history'),
+    path('changes', changes, name='serverdb_changes'),
+    path('changes_restore/<int:change_commit_id>', restore_deleted,
+         name='serverdb_restore_deleted'),
+    path('history', history, name='serverdb_history'),
 ]
