1720c05f447f37eb7f4402df1518a9b4cec26fd5
Daniel Kröger <daniel.kroeger@innogames.com>
2020-06-23 14:27:24 +0200

    cleanup(adminapi): Remove deprecated functions
    - Remove deprecated function adminapi.auth
    - Remove deprecated function adminapi.set_timeout

--- adminapi/__init__.py
+++ adminapi/__init__.py
@@ -1,17 +1,4 @@
 """Serveradmin - adminapi
 
-Copyright (c) 2018 InnoGames GmbH
+Copyright (c) 2020 InnoGames GmbH
 """
-
-from adminapi.request import Settings
-
-
-# XXX Deprecated
-def auth(auth_token=None):
-    if auth_token:
-        Settings.auth_token = auth_token
-
-
-# XXX Deprecated
-def set_timeout(timeout, what='api'):
-    Settings.timeout = timeout
