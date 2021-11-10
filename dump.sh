ebc790701971c1dfd4f444cf4a5a9983f948fe84
Daniel Kröger <daniel.kroeger@innogames.com>
2021-11-10 12:51:23 +0100

    Rename variable PRODUCTION_DB to REMOTE_DB
    REMOTE_DB makes it more clear that this is just a remote host and not
    necessarily the production database.

--- dump.sh
+++ dump.sh
@@ -2,7 +2,7 @@
 
 psql -h localhost -U "$POSTGRES_USER" -d postgres -c "drop database if exists $POSTGRES_DB;"
 psql -h localhost -U "$POSTGRES_USER" -d postgres -c "create database $POSTGRES_DB;"
-ssh "$PRODUCTION_DB" \
+ssh "$REMOTE_DB" \
     "pg_dump -O -x --exclude-table-data='serverdb_*' -d serveradmin | gzip -9 -c " \
     | gunzip -c \
     | psql -h localhost -U "$POSTGRES_USER" -d "$POSTGRES_DB"
