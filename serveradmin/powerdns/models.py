e69bb0aec546bd099bd5c21908542e4174939aee
Daniel Kröger <daniel.kroeger@innogames.com>
2021-12-03 14:19:20 +0100

    Add Record model to access PowerDNS records

--- serveradmin/powerdns/models.py
+++ serveradmin/powerdns/models.py
@@ -20,3 +20,32 @@ class Domain(models.Model):
     class Meta:
         managed = False
         db_table = 'domains'
+
+
+class Record(models.Model):
+    """PowerDNS Record
+
+    Model to access records in the PowerDNS db.
+    """
+    TYPES = [
+        ('A', 'A'),
+        ('AAAA', 'AAAA'),
+        ('CNAME', 'CNAME'),
+        ('TXT', 'TXT'),
+        ('SSHFP', 'SSHFP'),
+        ('SOA', 'SOA'),
+        ('MX', 'MX'),
+        ('PTR', 'PTR'),
+        ('NS', 'NS'),
+    ]
+
+    id = models.BigIntegerField(primary_key=True)
+    domain_id = models.ForeignKey('Domain', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, default=None)
+    type = models.CharField(max_length=10, default=None, choices=TYPES)
+    content = models.CharField(max_length=65535, default=None)
+    ttl = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'records'
