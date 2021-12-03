922052f2342850b41d42cdade772c0e56d53476a
Daniel Kröger <daniel.kroeger@innogames.com>
2021-12-03 12:09:30 +0100

    Add Domain model to access PowerDNS domains

--- serveradmin/powerdns/models.py
+++ serveradmin/powerdns/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Domain(models.Model):
+    """PowerDNS Domain
+
+    Model to access domain in the PowerDNS db.
+    """
+    TYPES = [
+        ('MASTER', 'MASTER'),
+        ('SLAVE', 'SLAVE'),
+        ('NATIVE', 'NATIVE'),
+    ]
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255, null=False)
+    master = models.CharField(max_length=128, default=None)
+    type = models.CharField(max_length=6, null=False, choices=TYPES)
+
+    class Meta:
+        managed = False
+        db_table = 'domains'
