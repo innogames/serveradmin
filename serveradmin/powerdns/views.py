4422376ac9df6fd76cdb0934ec5389a8f819152b
Daniel Kröger <daniel.kroeger@innogames.com>
2022-01-24 15:36:32 +0100

    Add records over page
    Add page to display PowerDNS records mapping configuration and PowerDNS
    records in PowerDNS database.

--- serveradmin/powerdns/views.py
+++ serveradmin/powerdns/views.py
@@ -3,7 +3,7 @@ from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from .models import Domain
+from .models import Domain, Record
 from .utils import get_settings, get_out_of_sync
 
 
@@ -27,3 +27,25 @@ def domains(request: HttpRequest) -> HttpResponse:
         'num_out_of_sync': len(get_out_of_sync('domain')),
     }
     return render(request, 'powerdns/domains.html', context)
+
+
+def records(request: HttpRequest) -> HttpResponse:
+    """Record overview page
+
+    :param request:
+    :return:
+    """
+    settings = get_settings('record')
+    if not settings:
+        messages.warning(request, 'No record mapping configured in settings!')
+        return render(request, 'empty.html')
+
+    paginator = Paginator(Record.objects.all(), 15)
+    page_obj = paginator.get_page(request.GET.get('page'))
+
+    context = {
+        'settings': settings,
+        'records': page_obj,
+        'num_out_of_sync': len(get_out_of_sync('record')),
+    }
+    return render(request, 'powerdns/records.html', context)
