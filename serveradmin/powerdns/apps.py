55725804c87568e64fe011146cac9dc7421d6cfd
Daniel Kröger <daniel.kroeger@innogames.com>
2022-02-11 15:02:06 +0100

    Setup signals module for powerdns app

--- serveradmin/powerdns/apps.py
+++ serveradmin/powerdns/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PowerdnsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'serveradmin.powerdns'
+
+    def ready(self):
+        from . import signals # noqa
