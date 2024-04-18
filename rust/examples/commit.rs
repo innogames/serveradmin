9ae9a3b0cbea430b2bda0a69518f6d9ad24ce22a
Yannik_Sc <yannik@yannik-sc.de>
2024-04-19 01:27:38 +0200

    Reformat code

--- rust/examples/commit.rs
+++ rust/examples/commit.rs
@@ -6,7 +6,8 @@ use adminapi::query::Query;
 #[tokio::main]
 pub async fn main() -> anyhow::Result<()> {
     env_logger::init();
-    let mut server = Query::builder().filter("hostname", regexp(".*payment-staging.*"))
+    let mut server = Query::builder()
+        .filter("hostname", regexp(".*payment-staging.*"))
         .filter("os", not(empty()))
         .restrict(["hostname", "responsible_admin", "os"])
         .build()
@@ -16,19 +17,19 @@ pub async fn main() -> anyhow::Result<()> {
         .pop()
         .ok_or(anyhow::anyhow!("No servers returned"))?;
 
-    server.set("os", "bookworm")?
+    server
+        .set("os", "bookworm")?
         .add("responsible_admin", "yannik.schwiegerr")?
         .remove("responsible_admin", "yannik.schwieger")?;
 
     let mut new_sg = new_object("service_group").await?.result;
-    new_sg.set("hostname", "yannik-adminapi-rs-2.test.sg")
+    new_sg
+        .set("hostname", "yannik-adminapi-rs-2.test.sg")
         .set("project", "test")
         .add("responsible_admin", "yannik.schwieger")
         .add("protocol_ports_inbound", "tcp443");
 
-    let changes = Commit::new()
-        .update(server.changeset())
-        .create(new_sg);
+    let changes = Commit::new().update(server.changeset()).create(new_sg);
     println!("{}", serde_json::to_string_pretty(&changes)?);
 
     let response = commit_changes(&changes).await?;
