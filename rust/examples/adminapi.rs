92c34292ba5edc1e7497bd3e62a357c0cf35562d
Yannik_Sc <yannik@yannik-sc.de>
2024-04-25 16:08:44 +0200

    Add cli parser, Identify api_call functions

--- rust/examples/adminapi.rs
+++ rust/examples/adminapi.rs
@@ -1,34 +1,24 @@
-use adminapi::api::new_object;
-use adminapi::filter::*;
+use adminapi::cli::parse_filter_args;
 use adminapi::query::Query;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
 
+    let mut args = std::env::args();
+    args.next();
+
+    let filters = parse_filter_args(args)?;
+
     let query = Query::builder()
-        .filter("hostname", regexp(".*payment-staging.*"))
-        .filter("os", not(empty()))
+        .filters(filters)
         .restrict(["hostname", "responsible_admin", "os"])
         .build();
 
-    // The fields are unused, but this example still should show, that you can have the query return structured data
-    #[allow(dead_code)]
-    #[derive(Clone, Debug, serde::Deserialize)]
-    struct MyServer {
-        hostname: String,
-        os: String,
-        responsible_admin: Vec<String>,
-    }
-
-    let servers = query.request_typed::<MyServer>().await?;
+    let servers = query.request().await?;
     for server in servers.into_iter() {
         println!("{server:#?}");
     }
-
-    let obj = new_object("service_group").await?;
-
-    println!("{obj:#?}");
 
     Ok(())
 }
