e041d6d9591077ffa58714d2cdf582da8bb6dc2b
Daniel Kröger <daniel.kroeger@innogames.com>
2025-07-22 17:24:49 +0200

    Codereview: Improve readability of reason for  boolean argument

--- adminapi/request.py
+++ adminapi/request.py
@@ -130,7 +130,7 @@ def calc_app_id(auth_token):
 def send_request(endpoint, get_params=None, post_params=None):
     for retry in reversed(range(Settings.tries)):
         request = _build_request(endpoint, get_params, post_params)
-        response = _try_request(request, bool(retry))
+        response = _try_request(request, retry != 0)
         if response:
             break
 
