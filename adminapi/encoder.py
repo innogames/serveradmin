f3125f07237236410009f06c1f9462374605a5c5
Daniel Kröger <daniel.kroeger@innogames.com>
2025-10-24 11:44:45 +0200

    Add support for all attribute types

--- adminapi/encoder.py
+++ adminapi/encoder.py
@@ -1,4 +1,7 @@
 import json
+import datetime
+import ipaddress
+import netaddr
 
 from adminapi.dataset import MultiAttr
 
@@ -7,6 +10,14 @@ class ServeradminJSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, MultiAttr):
             return list(obj)
+        elif isinstance(obj, datetime.date):
+            return obj.strftime('%Y-%m-%d')
+        elif isinstance(obj, datetime.datetime):
+            return obj.strftime('%Y-%m-%d %H:%M:%S%z')
+        elif isinstance(obj, ipaddress.IPv4Address) or isinstance(obj, ipaddress.IPv6Address):
+            return str(obj)
+        elif isinstance(obj, netaddr.EUI):
+            return str(obj)
 
         return json.JSONEncoder.default(self, obj)
 
