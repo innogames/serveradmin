1d86e6ab6259b7628065e30b3fb3961a2986c6da
Daniel Kröger <daniel.kroeger@innogames.com>
2026-07-01 15:22:29 +0200

    Improve security of DB dump script
    - Do not prompt for password anymore
    - Disable production accounts after dump
    - Change tokens after dump to prevent accidental leak

--- dump.sh
+++ dump.sh
@@ -1,8 +1,20 @@
 #!/bin/bash -e
 
-psql -h localhost -U "$POSTGRES_USER" -d postgres -c "drop database if exists $POSTGRES_DB;"
-psql -h localhost -U "$POSTGRES_USER" -d postgres -c "create database $POSTGRES_DB;"
+export PGPASSWORD="$POSTGRES_PASSWORD"
+
+sql() {
+    psql -h localhost -U "$POSTGRES_USER" -d $1 -c "$2"
+}
+
+sql postgres "drop database if exists $POSTGRES_DB;"
+sql postgres "create database $POSTGRES_DB;"
 ssh "$REMOTE_DB" \
     "pg_dump -O -x --exclude-table-data='serverdb_*' -d serveradmin | gzip -9 -c " \
     | gunzip -c \
     | psql -h localhost -U "$POSTGRES_USER" -d "$POSTGRES_DB"
+
+
+# To have less of a risk of accessing production and preventing token leaks
+# we better disable applications and change tokens
+sql "$POSTGRES_DB" 'update apps_application set disabled = true;'
+sql "$POSTGRES_DB" "update apps_application set auth_token = substr(md5(random()::text || id::text), 1, 25);"
